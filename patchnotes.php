725a3e68efb94cedba91701490ff22f9b2f4c872
brosaz <brosaz@osaxis.fr>
2019-05-14 16:43:10 +0200

    Patch notes : Update

--- patchnotes.php
+++ patchnotes.php
@@ -24,6 +24,21 @@
 	<div>
 		<p>Ce module est développé par Traumination, pour toute suggestion merci de passer par le discord de la VCT : <a href="https://discord.gg/cqPUDC7">https://discord.gg/cqPUDC7</a></p>
 	</div>
+	<h4>0.9.4</h4>
+	<ul>
+		<li>
+			Ultimate bravery opérationnel
+		</li>
+		<li>
+			Résumé de profil opérationnel
+		</li>
+		<li>
+			Résumé de profil : choix de skin possible
+		</li>
+		<li>
+			Icone "Pas d'item" fixe
+		</li>
+	</ul>
 	<h4>0.9.3</h4>
 	<ul>
 		<li>
